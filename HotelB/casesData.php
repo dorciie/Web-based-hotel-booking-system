d87c796fdfc6f1cb62f2a2eeb0a0fe067b61ae99
ayeshalee <62678591+ayeshalee@users.noreply.github.com>
2022-01-08 01:52:06 +0800

    Filter district cases

--- HotelB/casesData.php
+++ HotelB/casesData.php
@@ -50,14 +50,75 @@ foreach ($rows as $row) {
 array_shift($data);
 array_pop($data);
 
-$cluster = [];
+// $active_cluster =[];
+$main = [];
+$district_cases_active  = [];
+// $c= [];
 
 /**
  * Collect all dates available in cases_malaysia.csv
  * 
  */
 for ($i = 0; $i < count($data); $i++) {
-    array_push($cluster, $data[$i][9]);
+
+    /** 1. filter row to get active cluster based on "active" status (column 6)*/
+    if ($data[$i][6] == "active") {
+        
+        $temp = [];
+
+        /** 2. get only column district (column 2) */
+        // $temp["district"] = $data[$i][2];
+
+        /** 3. get column cases active (column 9) */
+        $temp["cases_active"] = $data[$i][9];
+
+        /** 4. replace all delimiter, 'DAN', 'AND' in columnn district to standard comma */
+        $pattern1 = "/ & /i";
+        $pattern2 = "/ dan /i";
+        $pattern3 = "/, /i";
+        $pattern4 = "/, dan /i";
+        $pattern5 = "/, and /i";
+        $pattern6 = "/ and /i";
+        $replace = preg_replace(array($pattern1, $pattern2, $pattern3, $pattern4, $pattern5, $pattern6), ",", $data[$i][2]);
+
+        /** 5. split disctrict by delimiter */
+        $rows = explode(",", $replace);
+        $split = [];
+        foreach ($rows as $row) {
+            array_push($split, $row);
+        }
+        $temp["district"] = $split;
+
+        /** 6. count number of disctrict */
+        $temp["district_count"] = count($split);
+
+        /** 7. average active cases per cluster _cases_per_district */
+        $temp["average"] = $data[$i][9] / count($split);
+
+        array_push($district_cases_active, $temp);
+    }
+}
+
+
+/** 8. total active cases group by district */
+$district_FINAL = [];
+
+for ($i = 0; $i < count($district_cases_active); $i++) {
+
+    $district = $district_cases_active[$i]["district"];
+
+    $district_count = $district_cases_active[$i]["district_count"];
+
+    $average = $district_cases_active[$i]["average"];
+
+    for ($j = 0; $j < $district_count; $j++) {
+
+        if (!array_key_exists($district[$j], $district_FINAL)) {
+            $district_FINAL[$district[$j]] =  $average;
+        } else {
+            $district_FINAL[$district[$j]] +=  $average;
+        }
+    }
 }
 
 // $cases_malaysia = [];
@@ -93,6 +154,6 @@ for ($i = 0; $i < count($data); $i++) {
  * Encode the cases_malaysia as a JSON.
  * 
  */
-$cases_malaysia = json_encode($cluster, JSON_PRETTY_PRINT);
+$cases_by_district = json_encode($district_FINAL, JSON_PRETTY_PRINT);
 
-echo $cases_malaysia;
+echo $cases_by_district;
