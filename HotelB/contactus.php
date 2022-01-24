83c4a3b69fbb1dd208c1ee6d6b406ab76670bd6b
qielarshdn <aqielarshdn99@gmail.com>
2022-01-25 00:06:36 +0800

    Updated contactus.php
    updated UI contactus.php

--- HotelB/contactus.php
+++ HotelB/contactus.php
@@ -7,185 +7,206 @@
   <script src="https://kit.fontawesome.com/c32adfdcda.js" crossorigin="anonymous"></script>
 
   <style>
-      @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap');
-
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  font-family: 'Poppins', sans-serif;
-}
-
-body {
-  min-height: 100vh;
-  width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background-image: url(https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940);
-  background-size: cover;
-  background-position: center;
-  position: relative;
-}
-
-body::after {
-  content: "";
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  background-color: rgba(0,0,0,0.8);
-}
-
-section {
-  position: relative;
-  z-index: 3;
-  padding-top: 50px;
-  padding-bottom: 50px;
-}
-
-.container {
-  max-width: 1080px;
-  margin-left: auto;
-  margin-right: auto;
-  padding-left: 20px;
-  padding-right: 20px;
-}
-
-.section-header {
-  margin-bottom: 50px;
-  text-align: center;
-}
-
-.section-header h2 {
-  color: #FFF;
-  font-weight: bold;
-  font-size: 3em;
-  margin-bottom: 20px;
-}
-
-.section-header p {
-  color: #FFF;
-}
-
-.row  {
-  display: flex;
-  flwx-wrap: wrap;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.contact-info {
-  width: 50%;
-  margin: auto;
-}
-
-.contact-info-item {
-  display: flex;
-  margin: auto;
-  /* width: 100%; */
-}
-
-.contact-info-icon {
-  height: 70px;
-  width: 70px;
-  background-color: #fff;
-  text-align: center;
-  border-radius: 50%;
-}
-
-.contact-info-icon i {
-  font-size: 30px;
-  line-height: 70px;
-  width: 70px;
-}
-
-.contact-info-content {
-  margin-left: 20px;
-}
-
-.contact-info-content h4 {
-  color: #1da9c0;
-  font-size: 1.4em;
-  margin-bottom: 5px;
-}
-
-.contact-info-content p {
-  color: #FFF;
-  font-size: 1em;
-}
-
-
-@media (max-width: 991px) {
-  section {
-    padding-top: 50px;
-    padding-bottom: 50px;
+  body {
+    /* background: #f2f2f2; */
+    background-image: url(https://wallpapercave.com/wp/wp1846069.jpg);
+    font-family: 'proxima-nova-soft', sans-serif;
+    font-size: 14px;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
-  
-  .row {
-    flex-direction: column;
+  .post-module {
+    position: relative;
+    z-index: 1;
+    display: block;
+    background: #ffffff;
+    min-width: 270px;
+    height: 470px;
+    -webkit-box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+    -moz-box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+    box-shadow: 0px 1px 2px 0px rgba(0, 0, 0, 0.15);
+    -webkit-transition: all 0.3s linear 0s;
+    -moz-transition: all 0.3s linear 0s;
+    -ms-transition: all 0.3s linear 0s;
+    -o-transition: all 0.3s linear 0s;
+    transition: all 0.3s linear 0s;
+    border-radius: 5px;
   }
-  
-  .contact-info {
-    margin-bottom: 40px;
+  .post-module:hover,
+  .hover {
+    -webkit-box-shadow: 0px 1px 35px 0px rgba(0, 0, 0, 0.3);
+    -moz-box-shadow: 0px 1px 35px 0px rgba(0, 0, 0, 0.3);
+    box-shadow: 0px 1px 35px 0px rgba(0, 0, 0, 0.3);
+  }
+  .post-module:hover .thumbnail img,
+  .hover .thumbnail img {
+    -webkit-transform: scale(1.1);
+    -moz-transform: scale(1.1);
+    transform: scale(1.1);
+    opacity: .6;
+  }
+  .post-module .thumbnail {
+    background: #000000;
+    height: 400px;
+    overflow: hidden;
+  }
+
+  .post-module .thumbnail img {
+    display: block;
+    width: 120%;
+    -webkit-transition: all 0.3s linear 0s;
+    -moz-transition: all 0.3s linear 0s;
+    -ms-transition: all 0.3s linear 0s;
+    -o-transition: all 0.3s linear 0s;
+    transition: all 0.3s linear 0s;
+  }
+  .post-module .post-content {
+    position: absolute;
+    bottom: 0;
+    background: #ffffff;
     width: 100%;
+    padding: 30px;
+    -webkti-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    -webkit-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+    -moz-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+    -ms-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+    -o-transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+    transition: all 0.3s cubic-bezier(0.37, 0.75, 0.61, 1.05) 0s;
+    border-radius: 5px;
   }
-  
-}
-</style>
+  .post-module .post-content .category {
+    position: absolute;
+    top: -34px;
+    left: 0;
+    background: #4dc193;
+    padding: 10px 15px;
+    color: #ffffff;
+    font-size: 14px;
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+  .post-module .post-content .title {
+    margin: 0;
+    padding: 0 0 10px;
+    color: #333333;
+    font-size: 26px;
+    font-weight: 700;
+  }
+  .post-module .post-content .sub_title {
+    margin: 0;
+    padding: 0 0 20px;
+    color: #e74c3c;
+    font-size: 20px;
+    font-weight: 400;
+  }
+  .post-module .post-content .description {
+    display: none;
+    color: #666666;
+    font-size: 14px;
+    line-height: 1.8em;
+  }
+  .post-module .post-content .post-meta {
+    margin: 30px 0 0;
+    color: #999999;
+  }
+
+  .post-module .post-content .post-meta a {
+    color: #999999;
+    text-decoration: none;
+  }
+
+  .container {
+    max-width: 800px;
+    min-width: 640px;
+    margin: 0 auto;
+    border-radius: 5px;
+  }
+  .container:before,
+  .container:after {
+    content: '';
+    display: block;
+    clear: both;
+  }
+  .container .column {
+    width: 50%;
+    padding: 0 25px;
+    -webkti-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+    float: left;
+  }
+  .container .column .demo-title {
+    margin: 0 0 15px;
+    color: #666666;
+    font-size: 18px;
+    font-weight: bold;
+    text-transform: uppercase;
+  }
+  .container .info {
+    width: 300px;
+    margin: 50px auto;
+    text-align: center;
+  }
+  .container .info h1 {
+    margin: 0 0 15px;
+    padding: 0;
+    font-size: 24px;
+    font-weight: bold;
+    color: #333333;
+  }
+  .container .info span {
+    color: #666666;
+    font-size: 12px;
+  }
+  .container .info span a {
+    color: #000000;
+    text-decoration: none;
+  }
+  .container .info span .fa {
+    color: #e74c3c;
+  }
+
+  </style>
+  <script>
+    $(window).load(function() {
+      $('.post-module').hover(function() {
+        $(this).find('.description').stop().animate({
+          height: "toggle",
+          opacity: "toggle"
+        }, 300);
+      });
+    });
+  </script>
+
+
 </head>
 <body>
-  
-  <section>
-    
-    <div class="section-header">
-      <div class="container">
-        <h2>Contact Us</h2>
-        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+  <div class="container">
+      <div class="info">
+          <h1>Contact Us </h1>
+      <!-- Normal Demo-->
+        <div class="column">
+          <!-- Post-->
+          <div class="post-module">
+              <!-- Thumbnail-->
+              <!-- <div class="thumbnail">
+                  <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/169963/photo-1429043794791-eb8f26f44081.jpeg" />
+              </div> -->
+              <!-- Post Content-->
+              <div class="post-content">
+                  <div class="category">Hotel Contact Details</div>
+                  <h1 class="title">Hotel Name</h1>
+                  <p class="address">Address</p>
+                  <p class="phone">Phone Number</p>
+              </div>
+          </div>
+        </div>
+      
+      
       </div>
-    </div>
-    
-    <div class="container">
-      <!-- <div class="row"> -->
-        
-        <div class="contact-info">
-          <div class="contact-info-item">
-            <div class="contact-info-icon">
-              <i class="fas fa-home"></i>
-            </div>
-            
-            <div class="contact-info-content">
-              <h4>Address</h4>
-              <p>Kolej Kediaman Ke-13,<br/> UM, Kuala Lumpur <br/>55060</p>
-            </div>
-          <!-- </div> -->
-          
-          <!-- <div class="contact-info-item"> -->
-            <div class="contact-info-icon">
-              <i class="fas fa-phone"></i>
-            </div>
-            
-            <div class="contact-info-content">
-              <h4>Phone</h4>
-              <p>012-3456789</p>
-            </div>
-          <!-- </div> -->
-          
-          <!-- <div class="contact-info-item"> -->
-            <div class="contact-info-icon">
-              <i class="fas fa-envelope"></i>
-            </div>
-            
-            <div class="contact-info-content">
-              <h4>Email</h4>
-             <p>smeproject@gmail.com</p>
-            </div>
-          <!-- </div> -->
-        </div> 
-      <!-- </div> -->
-    </div>
-  </section>
-  
+  </div>
+
 </body>
 </html>
